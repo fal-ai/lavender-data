784f290ec00f8da51e057816b108f36c82066d23
hanchchch <cndghks15@gmail.com>
2025-03-25 11:53:38 +0900

    fix: next dynamic route

--- packages/lavender-data-ui/src/lib/api/index.ts
+++ packages/lavender-data-ui/src/lib/api/index.ts
@@ -1,7 +1,11 @@
 import createClient from 'openapi-fetch';
 import type { paths } from './v1';
 
-export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
 export const client = createClient<paths>({
   baseUrl: API_URL,
+  fetch: (url) => {
+    return fetch(url, { cache: 'no-store' });
+  },
 });
