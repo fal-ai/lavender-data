3488f2d2178522ea8629bb53f27566af14c39610
hanchchch <cndghks15@gmail.com>
2025-06-18 09:30:38 +0900

    fix: ui api files path leading slash

--- ui/src/app/api/files/[...path]/route.ts
+++ ui/src/app/api/files/[...path]/route.ts
@@ -2,7 +2,7 @@ import { getDirect } from '@/lib/api';
 import { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
-  const path = request.nextUrl.pathname.replace('/api/files', '');
+  const path = request.nextUrl.pathname.replace('/api/files/', '');
   const response = await getDirect(`/files/${path}`);
 
   return response;
