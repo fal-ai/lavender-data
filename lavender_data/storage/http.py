bf9b856de7878cbb1a5d5ee48e4ab6b6300caff1
hanchchch <cndghks15@gmail.com>
2025-06-19 20:27:46 +0900

    feat: http_download options follow_redirects, show_progress

--- lavender_data/storage/http.py
+++ lavender_data/storage/http.py
@@ -1,6 +1,6 @@
 import httpx
 import os
-from pathlib import Path
+import tqdm
 from typing import Optional
 
 from lavender_data.storage.abc import Storage
@@ -9,14 +9,53 @@ from lavender_data.storage.abc import Storage
 MULTIPART_CHUNKSIZE = 1 << 23
 
 
+def http_download(
+    remote_path: str,
+    local_path: str,
+    *,
+    follow_redirects: bool = True,
+    show_progress: bool = False,
+) -> None:
+    with httpx.stream("GET", remote_path, follow_redirects=follow_redirects) as r:
+        content_length = r.headers.get("Content-Length")
+        if content_length is not None:
+            content_length = int(content_length)
+        else:
+            content_length = None
+
+        progress = tqdm.tqdm(
+            disable=not show_progress,
+            desc=f"Downloading {remote_path}",
+            total=content_length,
+            unit="B",
+            unit_scale=True,
+            unit_divisor=1024,
+            leave=False,
+        )
+        with open(local_path, "wb") as f:
+            for chunk in r.iter_bytes():
+                progress.update(len(chunk))
+                f.write(chunk)
+        progress.close()
+
+
 class HttpStorage(Storage):
     scheme = "http"
 
-    def download(self, remote_path: str, local_path: str) -> None:
-        with httpx.stream("GET", remote_path) as r:
-            with open(local_path, "wb") as f:
-                for chunk in r.iter_bytes():
-                    f.write(chunk)
+    def download(
+        self,
+        remote_path: str,
+        local_path: str,
+        *,
+        follow_redirects: bool = True,
+        show_progress: bool = False,
+    ) -> None:
+        http_download(
+            remote_path,
+            local_path,
+            follow_redirects=follow_redirects,
+            show_progress=show_progress,
+        )
 
     def upload(self, local_path: str, remote_path: str) -> None:
         raise NotImplementedError
@@ -34,11 +73,20 @@ class HttpStorage(Storage):
 class HttpsStorage(Storage):
     scheme = "https"
 
-    def download(self, remote_path: str, local_path: str) -> None:
-        with httpx.stream("GET", remote_path) as r:
-            with open(local_path, "wb") as f:
-                for chunk in r.iter_bytes():
-                    f.write(chunk)
+    def download(
+        self,
+        remote_path: str,
+        local_path: str,
+        *,
+        follow_redirects: bool = True,
+        show_progress: bool = False,
+    ) -> None:
+        http_download(
+            remote_path,
+            local_path,
+            follow_redirects=follow_redirects,
+            show_progress=show_progress,
+        )
 
     def upload(self, local_path: str, remote_path: str) -> None:
         raise NotImplementedError
