a022c31059c51bfc83f353bd747f86f26c3fce89
hanchchch <cndghks15@gmail.com>
2025-07-10 02:21:26 +0900

    fix: get current first

--- lavender_data/server/iteration/iteration_state/default.py
+++ lavender_data/server/iteration/iteration_state/default.py
@@ -480,6 +480,7 @@ class IterationState(IterationStateOps):
         filters = self._filters()
         categorizer = self._categorizer()
 
+        current = int(self.cache.incr(self._key("batch_count"), 1)) - 1
         global_sample_indices = []
         samples = []
         while len(samples) < max(batch_size, 1):
@@ -542,7 +543,6 @@ class IterationState(IterationStateOps):
                 samples.append(sample)
 
         cache_key = self._cache_key([i.index for i in global_sample_indices])
-        current = int(self.cache.incr(self._key("batch_count"), 1)) - 1
         return cache_key, ProcessNextSamplesParams(
             current=current,
             global_sample_indices=global_sample_indices,
